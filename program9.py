5b3badf088490671c332d2cb9693ca10bef0a860
Harsh Vaya <harsh.vaya127933@marwadiuniversity.ac.in>
2026-02-08 03:44:20 +0530

    This is program 9

---
+++ program9.py
@@ -0,0 +1,11 @@
+#name:harshvaya
+#enrolment.no:92400527205
+
+''' Write a program to input a number and display Table of that 
+number.  '''
+
+# Program 9
+num = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
