de99f630c57e2db8af4b7fbef7550150c29167e2
Harsh Vaya <harsh.vaya127933@marwadiuniversity.ac.in>
2026-02-08 03:42:42 +0530

    This is program 1

---
+++ program1.py
@@ -0,0 +1,17 @@
+
+#name:harshvaya
+#enrolment.no:92400527205
+
+'''Write a simple Python Program to INPUT two variables and print 
+Addition, Subtraction, Multiplication and Division of both numbers.'''
+
+
+
+# Program 1
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
