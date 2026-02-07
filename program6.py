daba95a0d49d55465255b6d5a8d7a34d98f13a8d
Harsh Vaya <harsh.vaya127933@marwadiuniversity.ac.in>
2026-02-08 03:43:38 +0530

    This is program 6

---
+++ program6.py
@@ -0,0 +1,14 @@
+#name:harshvaya
+#enrolment.no:92400527205
+
+''' Write a program to input a number and print whether it is Even or 
+Odd Number. '''
+
+
+# Program 6
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
