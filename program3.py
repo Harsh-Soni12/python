220b0eaef5fb65fefed41798ba59c0980204daf1
Harsh Vaya <harsh.vaya127933@marwadiuniversity.ac.in>
2026-02-08 03:43:09 +0530

    This is program 3

---
+++ program3.py
@@ -0,0 +1,14 @@
+
+#name:harshvaya
+#enrolment.no:92400527205
+
+'''Write a program to input Principal Amount, Rate and Year and 
+display Simple Interest.   '''
+
+# Program 3
+P = float(input("Enter Principal Amount: "))
+R = float(input("Enter Rate of Interest: "))
+T = float(input("Enter Time in Years: "))
+
+SI = (P * R * T) / 100
+print("Simple Interest:", SI)
