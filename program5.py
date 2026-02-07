a1f565afbf82bba40c8b65a5093432431f556fed
Harsh Vaya <harsh.vaya127933@marwadiuniversity.ac.in>
2026-02-08 03:43:29 +0530

    This is program 5

---
+++ program5.py
@@ -0,0 +1,12 @@
+#name:harshvaya
+#enrolment.no:92400527205
+
+'''Write a program to input radius of a circle, and print area of that 
+circle.'''
+
+#program5
+
+r=float(input("enter the redius:"))
+pi=3.14
+area=pi*r**2
+print("area is : " ,area)
