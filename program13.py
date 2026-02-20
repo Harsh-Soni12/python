75a9c3bebe8fe0c33b7fa19f68dac41e19fa38e9
Harsh Vaya <harsh.vaya127933@marwadiuniversity.ac.in>
2026-02-20 10:17:38 +0530

    This is program 13

---
+++ program13.py
@@ -0,0 +1,13 @@
+#name:harshvaya
+#enrolment.no:92400527205
+
+'''Write a program which will find all such numbers which are divisible 
+by 7 but are not a multiple of 5, between 2000 and 3200 (both 
+included).'''
+
+for n in range(2000, 3201):
+
+    if n % 7 == 0 and n % 5 != 0:
+        print(n, end=" ")
+        '''print(n)'''
+
