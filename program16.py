9a8b8c93e84b654ca9d06c882581d13eaf74a848
Harsh Vaya <harsh.vaya127933@marwadiuniversity.ac.in>
2026-04-07 16:58:24 +0530

    This is program 16

---
+++ program16.py
@@ -0,0 +1,16 @@
+
+ # name: harshvaya
+ # enrolment.no: 92400527205
+ 
+'''Write a program to create list in such a way that it should add 
+ square roots of number between 1 to n in the list... At the end, the 
+ list shall be displayed.  
+ Example : [1, 4, 9, 16, 25, ....] '''
+ 
+n = int(input("Enter a number: "))
+sqr = []
+ 
+for i in range(1, n+1):
+     sqr.append(i**2)
+ 
+ # display the list (missing part)
